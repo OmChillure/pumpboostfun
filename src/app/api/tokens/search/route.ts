4861438072ed08d80e65f9f984ab9020519b7828
omchillure <omchillure4@gmail.com>
2025-01-15 10:55:39 +0530

    fixed bug

--- src/app/api/tokens/search/route.ts
+++ src/app/api/tokens/search/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
 import clientPromise from '@/utils/db';
 
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
     try {
